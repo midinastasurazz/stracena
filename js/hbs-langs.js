30bff700a70cd18f05c063e1edf53796ef4d7c3b
midinastasurazz <mpavlovsky@Martins-MacBook-Pro.local>
2015-09-18 08:13:11 +0200

    fix Osvaleni, pozicia 7

--- js/hbs-langs.js
+++ js/hbs-langs.js
@@ -1,16 +1,16 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['langs'] = template({"1":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+templates['langs'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=container.escapeExpression;
+
   return "  	<li role=\"presentation\" id=\"lang"
-    + escapeExpression(((helper = (helper = helpers.englishName || (depth0 != null ? depth0.englishName : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"englishName","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.englishName || (depth0 != null ? depth0.englishName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"englishName","hash":{},"data":data}) : helper)))
     + "\"><a href=\"#\">"
-    + escapeExpression(((helper = (helper = helpers.localName || (depth0 != null ? depth0.localName : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"localName","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.localName || (depth0 != null ? depth0.localName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"localName","hash":{},"data":data}) : helper)))
     + "</a></li>\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1;
-  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.languages : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
-  if (stack1 != null) { return stack1; }
-  else { return ''; }
-  },"useData":true});
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.languages : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 })();
--- js/hbs-slider.js
+++ js/hbs-slider.js
@@ -1,22 +1,22 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['slider'] = template({"1":function(depth0,helpers,partials,data) {
-  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+templates['slider'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=container.escapeExpression;
+
   return "	<span style=\"float: left; width: 35px;\">\n		<a id=\"lookAtBodyFeature"
-    + escapeExpression(lambda((data && data.index), depth0))
+    + alias3(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
     + "\" class=\"btn btn-default btn-xs\"><i class=\"fa fa-video-camera fa-lg\"></i></a>\n	</span>\n	<span id=\"bodyFeatureLabel"
-    + escapeExpression(lambda((data && data.index), depth0))
+    + alias3(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
     + "\" style=\"float: left; width: 185px;\">"
-    + escapeExpression(lambda(depth0, depth0))
+    + alias3(container.lambda(depth0, depth0))
     + "</span>\n	<input type=\"range\" id=\"morph"
-    + escapeExpression(lambda((data && data.index), depth0))
+    + alias3(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
     + "\"  style=\"float: left; width: 50%; margin-top: 4px; padding-top: 0px; padding-bottom: 0px;\" min=\"1\" max=\"9\" step=\"1\" value=\"1\">\n	&nbsp;&nbsp;<span id=\"morph"
-    + escapeExpression(lambda((data && data.index), depth0))
+    + alias3(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
     + "Span\">1</span>\n	<br /><br />\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1;
-  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.features : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
-  if (stack1 != null) { return stack1; }
-  else { return ''; }
-  },"useData":true});
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.features : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 })();
